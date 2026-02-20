6f33b4a00375c799fa7f30f163a4d66a1242dfec
afonsopires <afonso.fda.pires@gmail.com>
2026-02-20 12:16:59 +0000

    Fixed logo length

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ st.set_page_config(
     layout="centered"
 )
 
-col1, col2 = st.columns([1, 4])
+col1, col2 = st.columns([1, 6])
 with col1:
-    st.image("assets/lasmall.png", width=80)
+    st.image("assets/lasmall.png", width=60)
 with col2:
     st.title("Soundvision PDF Extractor")
 
