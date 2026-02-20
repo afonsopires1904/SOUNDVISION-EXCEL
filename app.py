d32e59bdd773b291142833db83cdc353eb8c0e03
afonsopires <afonso.fda.pires@gmail.com>
2026-02-20 15:14:31 +0000

    Logo bug fixed

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ st.set_page_config(
 
 col1, col2 = st.columns([1, 6])
 with col1:
-    st.image("assets/logo.png", width=60)
+    st.image("assets/lasmall.png", width=60)
 with col2:
     st.title("Soundvision PDF Extractor")
 
